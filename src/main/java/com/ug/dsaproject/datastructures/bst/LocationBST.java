8b9715515ade19ba724e0133192943de0f7803b5
King1081 <papahquansah@gmail.com>
2026-08-18 10:26:44 +0000

    Implement insert, find, delete, and inorderTraversal methods in LocationBST class

--- src/main/java/com/ug/dsaproject/datastructures/bst/LocationBST.java
+++ src/main/java/com/ug/dsaproject/datastructures/bst/LocationBST.java
@@ -1,6 +1,8 @@
 package com.ug.dsaproject.datastructures.bst;
 
 import com.ug.dsaproject.model.Location;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * OWNER: Marfo Kwadwo Ampofo Senior
@@ -29,15 +31,64 @@ public class LocationBST {
     private Node root;
 
     public void insert(Location location) {
-        // TODO: implement
+        root = insertRec(root, location);
+    }
+
+    private Node insertRec(Node node, Location location) {
+        if (node == null) return new Node(location);
+        String key = location.getLocationId();
+        String nodeKey = node.data.getLocationId();
+        if (key.compareTo(nodeKey) < 0) {
+            node.left = insertRec(node.left, location);
+        } else if (key.compareTo(nodeKey) > 0) {
+            node.right = insertRec(node.right, location);
+        } else {
+            node.data = location; // replace existing
+        }
+        return node;
     }
 
     public Location find(String locationId) {
-        // TODO: implement
+        Node cur = root;
+        while (cur != null) {
+            int cmp = locationId.compareTo(cur.data.getLocationId());
+            if (cmp == 0) return cur.data;
+            if (cmp < 0) cur = cur.left; else cur = cur.right;
+        }
         return null;
     }
 
     public void delete(String locationId) {
-        // TODO: implement (handle 0/1/2-child cases)
+        root = deleteRec(root, locationId);
+    }
+
+    private Node deleteRec(Node node, String key) {
+        if (node == null) return null;
+        int cmp = key.compareTo(node.data.getLocationId());
+        if (cmp < 0) node.left = deleteRec(node.left, key);
+        else if (cmp > 0) node.right = deleteRec(node.right, key);
+        else {
+            if (node.left == null) return node.right;
+            if (node.right == null) return node.left;
+            // two children: find inorder successor
+            Node succ = node.right;
+            while (succ.left != null) succ = succ.left;
+            node.data = succ.data;
+            node.right = deleteRec(node.right, succ.data.getLocationId());
+        }
+        return node;
+    }
+
+    public List<Location> inorderTraversal() {
+        List<Location> res = new ArrayList<>();
+        inorderRec(root, res);
+        return res;
+    }
+
+    private void inorderRec(Node node, List<Location> out) {
+        if (node == null) return;
+        inorderRec(node.left, out);
+        out.add(node.data);
+        inorderRec(node.right, out);
     }
 }
